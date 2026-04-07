cf3d663525daa7c34d6c1dde781b1dead177f79c
Poetra <fajarkurniaputrasib@gmail.com>
2026-04-08 06:56:22 +0700

    Commit 7: Tugas 2 - Soal 1 & 2

--- pertemuan_06/lib/pages/item_page.dart
+++ pertemuan_06/lib/pages/item_page.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import '../models/item.dart'; 
 
 class ItemPage extends StatelessWidget {
   const ItemPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Item Page'),
       ),
-      body: const Center(
-        child: Text('Ini adalah Halaman Detail Item'),
+      body: Center(
+        child: Text(
+          'Nama Item: ${itemArgs.name}\nHarga: Rp${itemArgs.price}',
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
